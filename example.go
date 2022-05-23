5fca426e0b6a1f67f15c0040afe321f10ed3ce34
vadym <vadym.vikulin@gmail.com>
2022-05-23 13:56:42 +0300

    1. fixed example

--- example.go
+++ example.go
@@ -11,7 +11,7 @@ func main() {
 	if addr, err := net.ResolveUDPAddr("udp", "localhost:47008"); err != nil {
 		log.Fatalf("Unable to resolve address: %s", err)
 	} else {
-		go server(addr)
+		go server("localhost:47008")
 		time.Sleep(200 * time.Millisecond)
 		go client(addr)
 
@@ -19,14 +19,14 @@ func main() {
 	}
 }
 
-func server(addr *net.UDPAddr) {
+func server(addr string) {
 	if _, err := udt.ListenUDT("udp", addr); err != nil {
 		log.Fatalf("Unable to listen: %s", err)
 	}
 }
 
 func client(addr *net.UDPAddr) {
-	if _, err := udt.DialUDT("udp", nil, addr); err != nil {
+	if _, err := udt.DialUDT("udp", "0.0.0.0:0", addr, true); err != nil {
 		log.Fatalf("Unable to dial: %s", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/vikulin/go-udt
 go 1.17
 
 require github.com/furstenheim/nth_element v0.0.0-20190419153334-6ae3dd61145d
+
+require golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
