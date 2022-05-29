59d3dfc88e8ddc681a8b722d158bd1ca360dcd6c
vadym <vadym.vikulin@gmail.com>
2022-05-29 11:38:26 +0300

    1. added Listener interface

--- udt/interface.go
+++ udt/interface.go
@@ -24,3 +24,16 @@ type Connection interface {
 	SetWriteDeadline(t time.Time)
 	
 }
+
+type Listener interface {
+
+	// Accept waits for and returns the next connection to the listener.
+	Accept() (net.Conn, error)
+
+	// Close closes the listener.
+	// Any blocked Accept operations will be unblocked and return errors.
+	Close() error
+
+	
+	Addr() net.Addr
+}
